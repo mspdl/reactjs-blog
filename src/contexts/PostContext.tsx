338e4d684c06ae1bf64947ca6d44919fc10f7872
Morgan <morgan.espindola@gmail.com>
2024-02-01 15:48:04 -0300

    Persisting Context on LocalStorage

--- src/contexts/PostContext.tsx
+++ src/contexts/PostContext.tsx
@@ -5,8 +5,12 @@ import {
   ReactNode,
   createContext,
   useContext,
+  useEffect,
   useReducer,
+  useState,
 } from "react";
+
+const POSTS_KEY = "postContextContent";
 
 type PostContext = {
   posts: Post[];
@@ -15,14 +19,29 @@ type PostContext = {
 
 export const PostContext = createContext<PostContext | null>(null);
 
-export const PostProvider = ({ children }: { children: ReactNode }) => {
-  const [posts, dispatch] = useReducer(postReducer, []);
+export function PostProvider({ children }: { children: ReactNode }) {
+  const [isMounted, setIsMounted] = useState(false);
+  const [posts, dispatch] = useReducer(
+    postReducer,
+    typeof window !== "undefined"
+      ? JSON.parse(localStorage.getItem("posts") || "[]")
+      : []
+  );
+
+  useEffect(() => {
+    setIsMounted(true);
+    localStorage.setItem("posts", JSON.stringify(posts));
+  }, [posts]);
+
+  if (!isMounted) {
+    return null;
+  }
 
   return (
     <PostContext.Provider value={{ posts, dispatch }}>
       {children}
     </PostContext.Provider>
   );
-};
+}
 
 export const usePosts = () => useContext(PostContext);
